c5485229cf086727ff4bf5ae92b959cc30230f43
tmkayy <96301516+tmkayy@users.noreply.github.com>
2024-03-11 23:01:10 +0200

    added project

---
+++ JS Front End/JS Syntax Fundamentals/JS Syntax Fundamentals - Lab/05.MathOperations/MathOperations.js
@@ -0,0 +1,22 @@
+function math (num1, num2, s){
+  switch(s){
+    case '+':
+      console.log(num1+num2);
+      break;
+    case '-':
+      console.log(num1-num2);
+      break;
+    case '*':
+      console.log(num1*num2);
+      break;
+    case '/':
+      console.log(num1/num2);
+      break;
+    case '%':
+      console.log(num1%num2);
+      break;
+    case '**':
+      console.log(num1**num2);
+      break;
+  }
+}
